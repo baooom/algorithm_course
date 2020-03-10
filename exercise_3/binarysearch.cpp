9b387310dfb12a1334e8a3fa21d6d3378dd5e9f1
baooom <bao_m@foxmail.com>
2020-03-10 18:40:31 +0800

    二分查找2020.3.10

---
+++ exercise_3/binarysearch.cpp
@@ -0,0 +1,70 @@
+// #pragma GCC optimize(3,"Ofast","inline")
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <set>
+#include <stack>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <bitset>
+#include <assert.h>
+// #define getchar() (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1 << 21, stdin), p1 == p2) ? EOF : *p1++)
+// char buf[(1 << 21) + 1], * p1 = buf, * p2 = buf;
+// #define int long long
+#define lowbit(x) (x & (-x))
+#define lson root << 1, l, mid
+#define rson root << 1 | 1, mid + 1, r
+#define pb push_back
+typedef unsigned long long ull;
+typedef long long ll;
+typedef std::pair<ll, ll> pii;
+#define bug puts("BUG")
+const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+const int inf = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+const double eps = 1e-6;
+template <class T>
+inline void read(T &x)
+{
+    int sign = 1;char c = getchar();x = 0;
+    while (c > '9' || c < '0'){if (c == '-')sign = -1;c = getchar();}
+    while (c >= '0' && c <= '9'){x = x * 10 + c - '0';c = getchar();}
+    x *= sign;
+}
+#ifdef LOCAL
+    FILE* _INPUT=freopen("input.txt", "r", stdin);
+    // FILE* _OUTPUT=freopen("output.txt", "w", stdout);
+#endif
+using namespace std;
+const int maxn=1e5+10;
+int T[maxn];
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
+    {
+        scanf("%d", &T[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    int l = 1, r = n;
+    while (l <= r)
+    {
+        int mid = l + r >> 1;
+        if (T[mid] >= x)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+    if (l > n)
+        puts("0");
+    else
+        printf("%d\n", l);
+}
